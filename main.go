f8d51e467c5177f9c71c5abccf7027e03efe5dde
Lutfi Andriyanto <lutfiandri232@gmail.com>
2022-12-12 18:19:19 +0700

    feat(user): user controller (using gin)

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/lutfiandri/synapsis-ecommerce-api-challenge/controller"
+	"github.com/lutfiandri/synapsis-ecommerce-api-challenge/repository"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
 
 func main() {
-	fmt.Println("test")
+	dsn := "host=localhost user=postgres password=root dbname=ecommerce port=8081 sslmode=disable TimeZone=Asia/Jakarta"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	userRepository := repository.NewUserRepository(db)
+	userController := controller.NewUserController(&userRepository)
+
+	r := gin.Default()
+	userController.Route(r)
+
+	r.Run(":8080")
 }
